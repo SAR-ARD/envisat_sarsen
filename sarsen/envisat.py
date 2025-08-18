eb574ff37ee68f60a1a800911682e204947952af
Priit Pender <priit@alloca.ee>
2025-08-18 08:34:20 +0300

    Add support for external DORVOR orbit file.
    If no external orbit file is specified for the sarsen Envisat product, the processor uses internal orbit state vectors.
    
    At the moment this has great effect on the geolocation of the final product, needs to be clarified/studied further for geometric accuracy improvements.

--- sarsen/envisat.py
+++ sarsen/envisat.py
@@ -5,6 +5,7 @@ from typing import Any, List, Dict
 import numpy as np
 import pandas as pd
 import scipy.constants
+import datetime
 
 import xarray as xr
 
@@ -39,7 +40,7 @@ class EnvisatProduct:
     osv: xr.Dataset | None = None
     product_type: str = 'SLC'
 
-    def __init__(self, path: str, *args: Any, **kwargs: Any) -> None:
+    def __init__(self, path: str, osv_file=None, *args: Any, **kwargs: Any) -> None:
         """
         Initialize an Envisat product.
 
@@ -48,11 +49,12 @@ class EnvisatProduct:
         super().__init__(*args, **kwargs)
         self.path = path
         self.measurement = xr.open_dataset(path, engine='asar')
-        self.osv = self.compute_osv()
+        self.osv = self.compute_osv(osv_file)
 
     def beta_nought(self) -> xr.DataArray:
         cal_factor = self.measurement.metadata["direct_parse"]["cal_factor"]
-        return np.power(np.abs(self.measurement), 2) / cal_factor
+        cal_vector = self.measurement.metadata["direct_parse"]["cal_vector"]
+        return (np.power(np.abs(self.measurement), 2) / cal_factor) * cal_vector
 
     # def grid_parameters(
     #         self,
@@ -74,20 +76,72 @@ class EnvisatProduct:
         beta_nought = beta_nought.drop_vars(["pixel", "line"])
         return beta_nought
 
-    def compute_osv(self):
-        osv = self.measurement.attrs['metadata']["records"]["main_processing_params"]["orbit_state_vectors"]
+    def compute_osv(self, osv_file=None):
 
         azimuth_times: List[Any] = []
         positions: List[List[Any]] = [[], [], []]
         velocities: List[List[Any]] = [[], [], []]
-        for orbit in osv:
-            azimuth_times.append(orbit["state_vect_time_1"])
-            positions[0].append(orbit["x_pos_1"] * 1e-2)
-            positions[1].append(orbit["y_pos_1"] * 1e-2)
-            positions[2].append(orbit["z_pos_1"] * 1e-2)
-            velocities[0].append(orbit["x_vel_1"] * 1e-5)
-            velocities[1].append(orbit["y_vel_1"] * 1e-5)
-            velocities[2].append(orbit["z_vel_1"] * 1e-5)
+
+        if osv_file:
+            # osv file passed as argument, parse a DORVOR Envisat orbit file
+            orbit_content = None 
+            with open(osv_file, "r") as fp:
+                orbit_content = fp.read()
+            
+            orbit_content = orbit_content[1625:]
+            orbit_lines = orbit_content.split("\n")
+
+            first_line_time = self.measurement.attrs["metadata"]["first_line_time"]
+            first_line_dt = datetime.datetime.strptime(str(first_line_time)[:-3],'%Y-%m-%dT%H:%M:%S.%f')
+
+            osv_time_delta = 8
+
+            start_time = first_line_dt + datetime.timedelta(minutes=-osv_time_delta)
+            end_time = first_line_dt + datetime.timedelta(minutes=osv_time_delta)
+            for line in orbit_lines:
+                elems = line.split(" ")
+                if len(elems) < 10:
+                    continue
+                orbit_dt = elems[0] + " " + elems[1]
+                osv_timestamp = datetime.datetime.strptime(orbit_dt, "%d-%b-%Y %H:%M:%S.%f")
+
+                if osv_timestamp < start_time or osv_timestamp > end_time:
+                    continue
+
+                pos_x = float(elems[4])
+                pos_y = float(elems[5])
+                pos_z = float(elems[6])
+
+                vel_x = float(elems[7])
+                vel_y = float(elems[8])
+                vel_z = float(elems[9])
+
+                azimuth_times.append(np.datetime64(osv_timestamp))
+                positions[0].append(pos_x)
+                positions[1].append(pos_y)
+                positions[2].append(pos_z)
+
+                velocities[0].append(vel_x)
+                velocities[1].append(vel_y)
+                velocities[2].append(vel_z)
+            
+            assert(len(azimuth_times) > 5)
+
+        else:
+
+            osv = self.measurement.attrs['metadata']["records"]["main_processing_params"]["orbit_state_vectors"]
+
+            azimuth_times: List[Any] = []
+            positions: List[List[Any]] = [[], [], []]
+            velocities: List[List[Any]] = [[], [], []]
+            for orbit in osv:
+                azimuth_times.append(orbit["state_vect_time_1"])
+                positions[0].append(orbit["x_pos_1"] * 1e-2)
+                positions[1].append(orbit["y_pos_1"] * 1e-2)
+                positions[2].append(orbit["z_pos_1"] * 1e-2)
+                velocities[0].append(orbit["x_vel_1"] * 1e-5)
+                velocities[1].append(orbit["y_vel_1"] * 1e-5)
+                velocities[2].append(orbit["z_vel_1"] * 1e-5)
 
 
         return make_orbit(azimuth_times, positions, velocities)
