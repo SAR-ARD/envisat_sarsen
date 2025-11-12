hotfix: wrong order of operations

sarsen/apps.py
@@ -548,7 +548,9 @@ def envisat_terrain_correction(
         ]
         for layer_name, layer_data_array in layers_to_save.data_vars.items():
             layer_filename = f"{layers_urlpath}/{layer_name}.tif"
-            layer = layer_data_array.astype(np.float32).rio.to_raster(
+            layer = layer_data_array.astype(np.float32)
+            layer.rio.set_crs(dem_raster.rio.crs)
+            layer_delayed = layer.rio.to_raster(
                 layer_filename,
                 tiled=True,
                 blockxsize=output_chunks,
@@ -557,8 +559,7 @@ def envisat_terrain_correction(
                 num_threads="ALL_CPUS",
                 **to_raster_kwargs,
             )
-            layer.rio.set_crs(dem_raster.rio.crs)
-            delayed_layers.append(layer)
+            delayed_layers.append(layer_delayed)
 
     if enable_dask_distributed:
         maybe_delayed.compute()